33e24a7fee4075d536e68e19813b2430d02b1f38
afarica <57631090+afarica@users.noreply.github.com>
2019-11-20 11:06:58 +0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,7 +3,10 @@
 # Значение может быть как положительным, так и отрицательным. Если значение
 # положительное, элементы списка должны сдвигаться вправо, если отрицательное -
 # влево. Результат необходимо вывести на экран:
-source_data=list(input("Enter numbers separated by spaces:").split())
+source_data=list(input("Enter numbers separated by spaces:")
 shift=int(input('How much to shift?:'))
 for x in source_data:
-	print(int(x)+shift,end=' ')
+	a=int(ord(x))
+	b=a+shift
+	print(str(chr(b)),end='')
+
